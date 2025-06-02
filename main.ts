0cbd8232c29a2510b06e69e7a3f72f4fc4d1c2ed
Sn0f1a <snoflagralberg@icloud.com>
2025-06-02 15:14:20 +0200

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,17 @@
+let riktning = 1
+
+input.onPinPressed(TouchPin.P1, function () {
+    if (riktning == 1) {
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        basic.pause(2000)  // kör motor 2 sekunder framåt
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        riktning = -1
+    } else {
+        // backa motorn genom att använda annan signal
+        pins.digitalWritePin(DigitalPin.P2, 0)
+        basic.pause(2000)  // "bakåt" effekt – justera vid behov
+        pins.digitalWritePin(DigitalPin.P2, 1)
+        riktning = 1
+    }
+})
 
